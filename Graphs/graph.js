7ca81b76b1b3acc60312113cf1fd179693932fb5
vamshisai98 <42145575+vamshisai98@users.noreply.github.com>
2021-02-03 17:58:33 +0530

    remove edge

--- Graphs/graph.js
+++ Graphs/graph.js
@@ -8,4 +8,18 @@ class Graph {
       this.adjacencyList[vertex] = [];
     }
   }
+
+  addEdge(v1, v2) {
+    this.adjacencyList[v1].push(v2);
+    this.adjacencyList[v2].push(v1);
+  }
+
+  removeEdge(vertex1, vertex2) {
+    this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
+      (v) => v !== vertex2
+    );
+    this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
+      (v) => v !== vertex1
+    );
+  }
 }
