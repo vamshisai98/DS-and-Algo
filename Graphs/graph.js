aa68ed116ff710c5d8564ebd74931dd5fd4769ac
vamshisai98 <42145575+vamshisai98@users.noreply.github.com>
2021-02-03 18:08:00 +0530

    removeVertex

--- Graphs/graph.js
+++ Graphs/graph.js
@@ -22,4 +22,12 @@ class Graph {
       (v) => v !== vertex1
     );
   }
+
+  removeVertex(vertex) {
+    while (this.adjacencyList[vertex].length) {
+      const adjacentVertex = this.adjacencyList[vertex].pop();
+      this.removeEdge(vertex, adjacentVertex);
+    }
+    delete this.adjacencyList[vertex];
+  }
 }
